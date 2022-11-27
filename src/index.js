b46941e7507c2356455bfe5c464d91e73581073e
Miran San <miran.san@ozu.edu.tr>
2022-11-27 17:53:53 +0300

    odev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
 
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+// * [ ] `drag / drop`
 
 // Kodlar buraya gelecek!
+
+
+document.querySelector("h1").addEventListener("click", function (e) {
+    alert("Bana tıkladın!")
+});
+
+
+const h1YeniRenk = document.querySelector('h1');
+h1YeniRenk.addEventListener("mouseenter", (a) => {
+    a.target.style.color = 'purple';
+    setTimeout(() => {
+        a.target.style.color = "";
+    }, 500);
+}, false);
+
+
+// const farkliTiklama = document.querySelector ('.main-navigation.nav')
+// farkliTiklama.addEventListener("mouseover", altkey);
+// farkliTiklama.onmouseover = function(){myScript};
+
+
+const tumResimler = document.querySelectorAll('img');
+tumResimler.forEach((resim) => {
+    resim.addEventListener('mouseover', (event) => {
+        resim.style.filter = 'grayscale(100%)';
+    })
+}
+)
+
+
